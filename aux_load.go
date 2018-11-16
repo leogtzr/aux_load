21c9f0a834081be5a4229fd0c08b9486d3050704
leogtzr <leogutierrezramirez@gmail.com>
2018-11-16 14:39:39 -0700

    adding code skeleton

---
+++ aux_load.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+var x int = 0
+
+func main() {
+	m := http.NewServeMux()
+	s := http.Server{Addr: ":8000", Handler: m}
+
+	m.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Bye!")
+		s.Shutdown(context.Background())
+	})
+
+	m.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Bye!")
+		s.Shutdown(context.Background())
+	})
+
+	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world")
+	})
+
+	// main process ...
+	go func() {
+		for i := 0; i < 30; i++ {
+			fmt.Println(i)
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+
+	log.Printf("Finished")
+}
