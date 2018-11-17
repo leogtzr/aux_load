29cec536f4f9a0f376116dc34790a4febf9343b0
Leo <leogutierrezramirez@gmail.com>
2018-11-17 13:08:30 -0700

    changing dir

--- clean.sh
+++ clean.sh
@@ -4,6 +4,7 @@ set -o nounset
 set -o pipefail
 
 readonly working_dir=$(dirname $(readlink --canonicalize-existing "${0}"))
+cd "${working_dir}"
 
 go clean
 find "${working_dir}" -type f -name 'aux_load.log*' -print -delete
